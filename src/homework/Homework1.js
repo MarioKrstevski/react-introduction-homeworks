ab0b8a9856a8351dbee52922b7eb95c6ecc05209
Mario Krstevski <mariokrstevski@hotmail.com>
2023-06-06 00:24:47 +0200

    Two homeworks added

---
+++ src/homework/Homework1.js
@@ -0,0 +1,22 @@
+// Finish the components
+
+function Button(props) {
+  // https://react-bootstrap.github.io/components/buttons/
+  // variant (string) defines the colors
+  // outlined (boolean) - makes the button look outlined
+  // size (string) - changes the fontSize
+  // type code here
+}
+
+function Translate(props) {
+  // language (string) - choose two languages
+  // text - Text to translate
+  // Define a small collection of translations, Hello, Goodbye, I like programming, React is awesome
+}
+
+function WelcomeUser(params) {
+  // name (string)
+  // if name is missing or is empty, it should say: <p> Hello user
+  // if name is present, it should say: <h3> Hello {name}
+  // if name is your name, it should say: <h1> Hello Master
+}
---
+++ src/homework/Homework2.js
@@ -0,0 +1,25 @@
+/*
+
+1. Make a WeeklyTracker component that allows you to track multiple Habbit(s),
+Each habit will have a name and a goal (how many times per week it needs to be done)
+
+in the UI 
+-  add a button that will be used to add 1 points to a habit
+- prevent the user to add more points than the goal is
+- When the goal is reached, make the component glow ( any css, green text color is just fine)
+
+Here is how it would look like
+
+Brush teeth -- 0/14
+Work out -- 1/3
+
+
+2. Make a visual keyboard (make a button for all the letters), and at the top there should be 
+a placeholder (a div) where it will display the message. The message starts with "Today I will "
+and by clicking on each button the corresponding letter will be added at the end.
+
+(To solve the problem you don't need to add all letters, just enough to test the code logic, it should work
+the same with 2 buttons and with 200 buttons, but at the end add all the letters from the english alphabet and a few
+usefull ones as well (.,space!?)
+
+*/
